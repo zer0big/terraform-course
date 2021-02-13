4161ddabb5a787eef491df08f8461931694023a5
zerobig <zerobig.kim@gmail.com>
2021-02-13 19:08:01 +0900

    Disable AZ

--- 07_kubernetes_aks/main.tf
+++ 07_kubernetes_aks/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     node_count          = var.system_node_count
     vm_size             = "Standard_DS2_v2"
     type                = "VirtualMachineScaleSets"
-    availability_zones  = [1, 2, 3]
+//    availability_zones  = [1, 2, 3]
     enable_auto_scaling = false
   }
 
